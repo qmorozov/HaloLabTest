e7b8ae0bcb9d8f46eb64a3b421580764da05e146
Andrew Morozov <andr.morozov96@gmail.com>
2023-01-13 21:28:02 +0100

    scss modules

---
+++ src/Main.tsx
@@ -0,0 +1,11 @@
+import styles from './styles/pages/Main.module.scss';
+
+const Main = () => {
+  return (
+    <section className={styles.wrapper}>
+      <div className="container">Main</div>
+    </section>
+  );
+};
+
+export default Main;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Main from './Main';
 
 import './styles/global.scss';
 
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
